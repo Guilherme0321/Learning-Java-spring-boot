c5946232468bf984733310bb0c924fb4ee73ed5f
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2025-12-21 17:26:26 -0300

    feat: add CreateTransactionRequest DTO

---
+++ src/main/java/com/learningspringboot/finsmart/dto/TransactionRequestDTO.java
@@ -0,0 +1,72 @@
+package com.learningspringboot.finsmart.dto;
+
+import com.learningspringboot.finsmart.model.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TransactionRequestDTO {
+
+    private String userId;
+    private BigDecimal amount;
+    private String description;
+    private LocalDateTime date;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private TransactionType type;
+    private Long categoryId;
+
+    public TransactionRequestDTO() {};
+
+    public TransactionRequestDTO(
+            String userId,
+            BigDecimal amount,
+            String description,
+            LocalDateTime date,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt,
+            TransactionType type,
+            Long categoryId
+    ) {
+        this.userId = userId;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.type = type;
+        this.categoryId = categoryId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
