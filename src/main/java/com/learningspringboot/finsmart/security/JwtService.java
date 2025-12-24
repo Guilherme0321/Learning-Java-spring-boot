dcbcbe67d9f8ffc52be9fddf1fb8febc22b627b1
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2025-12-24 14:38:07 -0300

    add: add configuration for Jwt authentication

---
+++ src/main/java/com/learningspringboot/finsmart/security/JwtService.java
@@ -0,0 +1,28 @@
+package com.learningspringboot.finsmart.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+// TODO: Adicionar verificação de token
+@Component
+public class JwtService {
+    private final String SECRET_KEY = "I-am-a-tea-bot!";
+
+    // TODO: Adicionar mais parametros do usuário
+    public String generateToken(String username) {
+
+        int second = 1000;
+        int minute = second * 60;
+        int hour = minute * 60;
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + hour)
+                ).compact();
+    }
+
+}
