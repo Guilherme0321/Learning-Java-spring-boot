bb2d76f5d69573ca2c41a6d5961c84e1645de730
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2025-12-24 14:06:16 -0300

    refactor: using TransactionResonseDTO instead of Transaction

--- src/main/java/com/learningspringboot/finsmart/service/TransactionService.java
+++ src/main/java/com/learningspringboot/finsmart/service/TransactionService.java
@@ -1,6 +1,9 @@
 package com.learningspringboot.finsmart.service;
 
-import com.learningspringboot.finsmart.dto.TransactionRequestDTO;
+import com.learningspringboot.finsmart.dto.transaction.TransactionRequestDTO;
+import com.learningspringboot.finsmart.dto.transaction.TransactionResponseDTO;
+import com.learningspringboot.finsmart.exception.CategoryNotFoundException;
+import com.learningspringboot.finsmart.exception.TransactionNotFoundException;
 import com.learningspringboot.finsmart.model.Category;
 import com.learningspringboot.finsmart.model.Transaction;
 import com.learningspringboot.finsmart.repository.CategoryRepository;
@@ -24,7 +27,7 @@ public class TransactionService {
         this.transactionRepository = transactionRepository;
     }
 
-    public Transaction save(TransactionRequestDTO transactionRequestDTO) {
+    public TransactionResponseDTO save(TransactionRequestDTO transactionRequestDTO) {
 
         BigDecimal zero = BigDecimal.valueOf(0.0);
 
@@ -45,7 +48,7 @@ public class TransactionService {
         }
 
         Category category = categoryRepository.findById(transactionRequestDTO.getCategoryId())
-                .orElseThrow(() -> new RuntimeException("Categoria não encontrada!"));
+                .orElseThrow(() -> new CategoryNotFoundException(transactionRequestDTO.getCategoryId()));
 
         Transaction transaction = new Transaction(
                 transactionRequestDTO.getUserId(),
@@ -56,10 +59,50 @@ public class TransactionService {
                 category
         );
 
-        return transactionRepository.save(transaction);
+        Transaction result = transactionRepository.save(transaction);
+
+        return new TransactionResponseDTO(
+                result.getId(),
+                result.getCategory(),
+                result.getType(),
+                result.getDate(),
+                result.getAmount(),
+                result.getDescription()
+        );
     }
 
-    public List<Transaction> list() {
-        return transactionRepository.findAll();
+    public List<TransactionResponseDTO> list() {
+        List<Transaction> transactions = transactionRepository.findAll();
+        return transactions.stream()
+                .map(transaction -> {
+                    return new TransactionResponseDTO(
+                            transaction.getId(),
+                            transaction.getCategory(),
+                            transaction.getType(),
+                            transaction.getDate(),
+                            transaction.getAmount(),
+                            transaction.getDescription()
+                    );
+                }).toList();
+    }
+
+    public TransactionResponseDTO delete(Long id) {
+        if(id == null) {
+            throw new IllegalArgumentException("O campo ID deve estar preenchido!");
+        }
+
+        Transaction transactionToBeDeleted = transactionRepository.findById(id)
+                .orElseThrow(() -> new TransactionNotFoundException(id));
+
+        transactionRepository.delete(transactionToBeDeleted);
+
+        return new TransactionResponseDTO(
+                transactionToBeDeleted.getId(),
+                transactionToBeDeleted.getCategory(),
+                transactionToBeDeleted.getType(),
+                transactionToBeDeleted.getDate(),
+                transactionToBeDeleted.getAmount(),
+                transactionToBeDeleted.getDescription()
+        );
     }
 }
