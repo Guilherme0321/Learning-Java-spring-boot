8d7f2706a70154a8acfc293f8e6b5b23ebbfc0f1
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2025-12-24 14:38:33 -0300

    add: add UserService

---
+++ src/main/java/com/learningspringboot/finsmart/service/UserService.java
@@ -0,0 +1,29 @@
+package com.learningspringboot.finsmart.service;
+
+import com.learningspringboot.finsmart.model.User;
+import com.learningspringboot.finsmart.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+// TODO: Adicionar função para verificação do usuário
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
+
+    public User save(String username, String password) {
+        User user = new User(
+                username,
+                encoder.encode(password)
+        );
+
+        return userRepository.save(user);
+    }
+}
