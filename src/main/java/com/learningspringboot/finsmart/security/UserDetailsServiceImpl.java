5e90f0dec9d4e5cd1166ff9218566b6afb42f035
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2025-12-24 16:46:38 -0300

    add: implementation of the UserDetailsService interface

---
+++ src/main/java/com/learningspringboot/finsmart/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.learningspringboot.finsmart.security;
+
+import com.learningspringboot.finsmart.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository resRepository;
+
+    public UserDetailsServiceImpl(UserRepository resRepository) {
+        this.resRepository = resRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return resRepository.findByUsername(username)
+                .map(UserDetailsImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
+    }
+}
