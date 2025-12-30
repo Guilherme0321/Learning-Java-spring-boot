7f83e7ff4db6d7e61d002b36d2c9612dccfb072e
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2025-12-29 23:13:35 -0300

    feat: using UserValidator to valid the params

--- src/main/java/com/learningspringboot/finsmart/service/UserService.java
+++ src/main/java/com/learningspringboot/finsmart/service/UserService.java
@@ -1,24 +1,28 @@
 package com.learningspringboot.finsmart.service;
 
+import com.learningspringboot.finsmart.dto.user.UserValidator;
 import com.learningspringboot.finsmart.model.User;
 import com.learningspringboot.finsmart.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-// TODO: Adicionar função para verificação do usuário
+// TODO: adicioanr validator
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
+    private final UserValidator validator;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder encoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder encoder, UserValidator validator) {
         this.userRepository = userRepository;
         this.encoder = encoder;
+        this.validator = validator;
     }
 
     public User save(String username, String password) {
+        validator.validate(username, password);
 
         User user = new User();
         user.setUsername(username);
