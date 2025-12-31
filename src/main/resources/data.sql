4674c0c6e3ee63603bfce490887e1a48d2d751ba
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2025-12-31 17:12:56 -0300

    feat: adding just roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO users (username, password)
+INSERT INTO roles (name)
 VALUES
-    ('administrator', '$2a$12$URok/B/o/ugW5b3Df/AZquQ4sODXr1RSpKJNywoVfJNHwW6Ne6KyG')
-ON CONFLICT (username) DO NOTHING;
+    ('ADMIN'),
+    ('USER')
+ON CONFLICT (name) DO NOTHING;
