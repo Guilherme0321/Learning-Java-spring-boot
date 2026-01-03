39a2aa7c5ab09995260af2b67163023cdcaec696
Guilherme Oliveira <130414850+Guilherme0321@users.noreply.github.com>
2026-01-03 10:53:54 -0300

    feat: added constructor with username

--- src/main/java/com/learningspringboot/finsmart/exception/UserNotFoundException.java
+++ src/main/java/com/learningspringboot/finsmart/exception/UserNotFoundException.java
@@ -4,6 +4,10 @@ public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(Long id) {
         super("Usuário com id: " + id + " não encontrado");
     }
+
+    public UserNotFoundException(String username) {
+        super("Usuário com username: " + username + " não encontrado");
+    }
 }
 
 
